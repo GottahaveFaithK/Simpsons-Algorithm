10bbd4643ade05afd8476c89cf7b03a58046efe2
Faith Kirkham <faithkirkham12@gmail.com>
2026-02-07 13:21:06 -0700

    made notes for future needed tests

---
+++ tests/test_simpsons.py
@@ -0,0 +1,8 @@
+import math
+
+"""
+test overall accuracy: constant, linear, quadratic, cubic (these will all have exact solutions)
+test n validation: check if both the first and second pass give me an error value that is less than 1%...
+try to cause premature convergence??
+
+"""
